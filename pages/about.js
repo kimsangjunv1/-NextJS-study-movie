cadca5cc6556f8ef6d08827e8c0cad4a7da9c509
kimsangjunv1 <ingyoe64@gmail.com>
2023-08-01 17:33:20 +0900

    Test: Style Component 설치 확인용

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,14 @@
+import styled from "styled-components";
+
+const Test = styled.div`
+  background: red;
+  color: white;
+`;
+
 export default function Potato() {
   return (
     <>
+      <Test>빨간색 배경</Test>
       <p>나옴!</p>
     </>
   );
